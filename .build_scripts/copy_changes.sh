2efff084b1b861fb0225e71e31281032a575de07
Anna Bell <undefinedvalue@users.noreply.github.com>
2016-05-15 19:03:35 -0700

    Revert to copy all

--- .build_scripts/copy_changes.sh
+++ .build_scripts/copy_changes.sh
@@ -2,5 +2,5 @@
 
 mkdir "build"
 
-git diff-tree -r --no-commit-id --name-only --diff-filter=ACMRT HEAD www/ | xargs -I '{}' cp '{}' build
-
+#git diff-tree -r --no-commit-id --name-only --diff-filter=ACMRT HEAD www/ | xargs -I '{}' cp '{}' build
+cp -r www/* build
