14cba14bef3ceef10325b7cd21b60ccfe70ee0fd
Anna Bell <undefinedvalue@users.noreply.github.com>
2016-05-16 16:09:26 -0700

    Only upload changed files to s3

---
+++ .build_scripts/s3_upload.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import boto3
+import os
+from subprocess import check_output
+
+bucket_name = os.environ['S3_BUCKET']
+local_dir = 'www'
+
+s3 = boto3.resource('s3')
+bucket = s3.Bucket(bucket_name)
+
+git_ls = check_output(['git', 'ls-files', '-s', local_dir])
+
+hashes = [(os.path.relpath(n, local_dir), c, n) for (_, c, _, n) in [s.split() for s in git_ls.splitlines()]]
+
+for (filename, local_hash, path) in hashes:
+  obj = bucket.Object(filename)
+  obj.load()
+  s3_hash = obj.metadata.get('hash')
+
+  if s3_hash != local_hash:
+    print "{} differs, uploading".format(path)
+    obj.upload_file(path, ExtraArgs={'Metadata': {'hash': local_hash}})
